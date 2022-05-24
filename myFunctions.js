06a374db14d5b022b3110619adbd453f5a60a15b
MohammadTaji <mohamadtaji.mt@gmail.com>
2022-05-24 09:16:37 +0300

    Added modal to products page

--- myFunctions.js
+++ myFunctions.js
@@ -1,7 +1,23 @@
+// start modal code
+
 let eveTable = document.querySelectorAll(`.eve`);
+let modal = document.querySelector(`.products .modal`);
+let header = document.querySelector(`.products .modal h2`);
+let modalPara = document.querySelectorAll(`.products .modalPara `);
 
 eveTable.forEach((ele, i) => {
     ele.addEventListener(`click`, () => {
-        window.open(`/pages/${i + 1}.html`, `_blank`, `width=800,height=800,left=0,top=0`);
+        modal.classList.add(`hide`);
+        modalPara[i].style.display = "block";
+        header.textContent = document.querySelectorAll(`.eve td:first-child`)[i].textContent;
     })
 });
+
+
+function modalClose() {
+    modal.classList.remove(`hide`);
+    modalPara.forEach((el) => {
+        el.style.display = "none"
+    })
+}
+// end modal code
