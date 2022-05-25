8ea0d70730fffb4412ce78fb3b34dcfb5381138d
MohammadTaji <mohamadtaji.mt@gmail.com>
2022-05-26 01:23:42 +0300

    Added captcha to Req_Loan page

--- myFunctions.js
+++ myFunctions.js
@@ -95,3 +95,53 @@ function load() {
     document.getElementById(`resPayment`).value =
         `${amount().toFixed(2)} كل ${payment()} شهر `;
 }
+
+
+// Start Captcha
+
+const captcha = document.querySelector(".captcha"),
+    inputField = document.querySelector(".wrapper input"),
+    checkBtn = document.querySelector(".check-btn"),
+    statusTxt = document.querySelector(".status-text");
+//storing all captcha characters in array
+let allCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function getCaptcha() {
+    for (let i = 0; i < 6; i++) {
+        let randomCharacter = allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        captcha.innerText += ` ${randomCharacter}`;
+    }
+}
+getCaptcha();
+function removeContent() {
+    inputField.value = "";
+    captcha.innerText = "";
+}
+
+checkBtn.addEventListener("click", e => {
+    statusTxt.style.display = "block";
+    let inputVal = inputField.value.split('').join(' ');
+    if (inputVal == captcha.innerText) {
+        removeContent();
+        getCaptcha();
+        statusTxt.style.color = "#4db2ec";
+        statusTxt.innerText = "Nice! You don't appear to be a robot.";
+    } else if (inputVal == "") {
+        e.preventDefault();
+        inputField.focus();
+        statusTxt.style.color = "#ff0000";
+        statusTxt.innerText = "Enter captcha text Please";
+    }
+    else {
+        e.preventDefault();
+        inputField.focus();
+        statusTxt.style.color = "#ff0000";
+        statusTxt.innerText = "Captcha not matched. Please try again!";
+        removeContent();
+        getCaptcha();
+    }
+});
+
+// End Captcha
